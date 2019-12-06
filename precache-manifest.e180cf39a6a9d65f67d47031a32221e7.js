6b6f5f8476bfcbf985a014cef4a7aa88da468aef
Gmuggle <you@example.com>
2019-12-06 13:58:26 +0800

    Updates

---
+++ precache-manifest.e180cf39a6a9d65f67d47031a32221e7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f633e2fd5a8cf3fa516c3fcf683979d",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "bcd75279a22772c97961",
+    "url": "/drum-machine/static/css/2.a19fc868.chunk.css"
+  },
+  {
+    "revision": "666fe3e11ccafb0fc73f",
+    "url": "/drum-machine/static/css/main.918d0cd7.chunk.css"
+  },
+  {
+    "revision": "bcd75279a22772c97961",
+    "url": "/drum-machine/static/js/2.e159e625.chunk.js"
+  },
+  {
+    "revision": "666fe3e11ccafb0fc73f",
+    "url": "/drum-machine/static/js/main.aeb294c6.chunk.js"
+  },
+  {
+    "revision": "4f4cae8de84ab9fd73d9",
+    "url": "/drum-machine/static/js/runtime-main.082e828f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/drum-machine/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/drum-machine/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/drum-machine/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/drum-machine/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/drum-machine/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/drum-machine/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/drum-machine/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/drum-machine/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/drum-machine/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/drum-machine/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "cf48bcdb51b06e08119fba19df24e47e",
+    "url": "/drum-machine/static/media/yisell_sound_2008030716560441118_88011.cf48bcdb.mp3"
+  }
+]);
